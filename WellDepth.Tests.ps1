70b9f4f0869e25de5f9a41bf09814308bc590b5f
ChrisMagnuson <hourglasssand@gmail.com>
2017-08-09 15:32:37 -0400

    Remove wrongly thought out functions

--- WellDepth.Tests.ps1
+++ WellDepth.Tests.ps1
@@ -1,8 +1,6 @@
 ﻿Describe -Name "Temperature Conversions" -Fixture {    
     it -name "Test with starting value in Kelvin"  {
         Get-Temperature -Kelvin 32
-        $Kelvin = 32        
-        ConvertTo-Fahrenheit -Kelvin $Kelvin | select -Property Kelvin | should be $Kelvin
-        ConvertTo-Celsius -Kelvin $Kelvin | select -Property Kelvin | should be $Kelvin
+        Get-Temperature -Fahrenheit 90
     }
 }
