5324c84e2c8c61739f76999f92f8a59f072c2da4
ChrisMagnuson <hourglasssand@gmail.com>
2017-08-09 15:31:54 -0400

    Initial commit

---
+++ WellDepth.Tests.ps1
@@ -0,0 +1,8 @@
+﻿Describe -Name "Temperature Conversions" -Fixture {    
+    it -name "Test with starting value in Kelvin"  {
+        Get-Temperature -Kelvin 32
+        $Kelvin = 32        
+        ConvertTo-Fahrenheit -Kelvin $Kelvin | select -Property Kelvin | should be $Kelvin
+        ConvertTo-Celsius -Kelvin $Kelvin | select -Property Kelvin | should be $Kelvin
+    }
+}
---
+++ WellDepth.psm1
@@ -0,0 +1,79 @@
+﻿$LengthofTimeToEcho = 0.018
+$SpeedOfSound
+
+function Get-Temperature {
+    [CmdletBinding(DefaultParameterSetName="All")]
+    param (
+        [Parameter(Mandatory,ParameterSetName="Fahrenheit")]$Fahrenheit,
+        [Parameter(Mandatory,ParameterSetName="Celsius")]$Celsius,
+        [Parameter(Mandatory,ParameterSetName="Kelvin")]$Kelvin
+    )    
+    $TemperatureInKelvin = if ($Kelvin) { 
+        $Kelvin 
+    } elseif ($Fahrenheit) {
+        (($Fahrenheit + 459.67) * 5/9)
+    } elseif ($Celsius) {
+        ($Celsius + 273.15)
+    }
+
+    [PSCustomObject]@{} | 
+    Add-Member -MemberType NoteProperty -PassThru -Name Kelvin -Value $TemperatureInKelvin |
+    Add-Member -MemberType ScriptProperty -PassThru -Name Fahrenheit -Value {
+        ($this.Kelvin * 9/5 - 459.67)
+    } |
+    Add-Member -MemberType ScriptProperty -Name Celsius -PassThru -Value {
+        ($This.Kelvin + 273.15)
+    }
+    
+}
+
+function Get-ElkremDepthOfWell {
+    
+    $TemperatureInFahrenheit
+}
+
+function Get-SpeedOfSoundMetersPerSecond {
+    param (
+        $TemperatureInCelsius
+    )
+    331.4 + (.6 * $TemperatureInCelsius)
+}
+
+#function ConvertTo-Fahrenheit {
+#    [CmdletBinding(DefaultParameterSetName="All")]
+#    param (
+#        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName="Celsius")]$Celsius,
+#        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName="Kelvin")]$Kelvin
+#    )
+#    if ($Celsius) {
+#        Get-Temperature -Fahrenheit ((($Celsius * 9) / 5) + 32)
+#    } elseif ($Kelvin) {
+#        Get-Temperature -Fahrenheit ($Kelvin * 9/5 - 459.67)
+#    }
+#}
+#
+#function ConvertTo-Celsius {
+#    [CmdletBinding(DefaultParameterSetName="All")]
+#    param (
+#        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName="Fahrenheit")]$Fahrenheit,
+#        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName="Kelvin")]$Kelvin
+#    )
+#    if ($Fahrenheit) { 
+#        Get-Temperature -Celsius (($Fahrenheit - 32) * 5/9)
+#    } elseif ($Kelvin) {
+#        Get-Temperature -Celsius ($Kelvin + 273.15)
+#    }
+#}
+#
+#function ConvertTo-Kelvin {
+#    [CmdletBinding(DefaultParameterSetName="All")]
+#    param (
+#        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName="Celsius")]$Celsius,
+#        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName="Fahrenheit")]$Fahrenheit
+#    )
+#    if ($Celsius) { 
+#        Get-Temperature -Kelvin ($Celsius + 273.15)
+#    } elseif ($Fahrenheit) {
+#        Get-Temperature -Kelvin (($Fahrenheit + 459.67) * 5/9)
+#    }
+#}
